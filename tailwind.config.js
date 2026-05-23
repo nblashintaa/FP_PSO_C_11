0848f3fb02558ec8d0407b73609ad131f54f4368
nblashintaa <nabilashintaluthfia@gmail.com>
2026-05-23 19:41:57 +0700

    fix tailwind paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {
       backgroundImage: {
@@ -12,6 +15,7 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
       colors: {
         accent: {
           white: {
@@ -23,5 +27,6 @@ module.exports = {
       },
     },
   },
+
   plugins: [],
 };
